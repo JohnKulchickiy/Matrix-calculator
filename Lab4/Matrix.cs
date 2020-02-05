aa437b40e05573af216d146ad67f5923d3299455
Rostislav Kulchickiy <rokulchickiy@gmail.com>
2020-02-05 14:22:34 +0200

    Init

---
+++ Lab4/Matrix.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Matrix
+    {
+        static Random rnd = new Random();
+        private double[,] a;
+        public Matrix(int line = 3, int cologne = 3)
+        {
+            a = new double[line, cologne];
+            for (int x = 0; x < line; x++)
+            {
+                for (int y = 0; y < cologne; y++)
+                {
+                    a[x, y] = rnd.Next(0, 10);
+                }
+            }
+        }
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            if (a.a.GetLength(0) != b.a.GetLength(0) || a.a.GetLength(1) != b.a.GetLength(1))
+            {
+                return null;
+            }
+            else
+            {
+                Matrix res = new Matrix(a.a.GetLength(0), a.a.GetLength(1));
+                for (int x = 0; x < a.a.GetLength(0); x++)
+                {
+                    for (int y = 0; y < a.a.GetLength(1); y++)
+                    {
+                        res.a[x, y] = a.a[x, y] + b.a[x, y];
+                    }
+                }
+                return res;
+            }
+        }
+        public static Matrix operator -(Matrix a, Matrix b)
+        {
+            if (a.a.GetLength(0) != b.a.GetLength(0) || a.a.GetLength(1) != b.a.GetLength(1))
+            {
+                return null;
+            }
+            else
+            {
+                Matrix res = new Matrix(a.a.GetLength(0), a.a.GetLength(1));
+                for (int x = 0; x < a.a.GetLength(0); x++)
+                {
+                    for (int y = 0; y < a.a.GetLength(1); y++)
+                    {
+                        res.a[x, y] = a.a[x, y] - b.a[x, y];
+                    }
+                }
+                return res;
+            }
+        }
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            if (a.a.GetLength(1) != b.a.GetLength(0) || a.a.GetLength(0) != b.a.GetLength(1))
+            {
+                return null;
+            }
+            else
+            {
+                Matrix res = new Matrix(a.a.GetLength(0), b.a.GetLength(1));
+                for (int x = 0; x < a.a.GetLength(0); x++)
+                {
+                    for (int y = 0; y < b.a.GetLength(1); y++)
+                    {
+                        for (int k = 0; k < b.a.GetLength(0); k++)
+                        {
+                            res.a[x, y] += a.a[x, k] * b.a[k, y];
+                        }
+                    }
+                }
+                return res;
+            }
+        }
+        public static Matrix operator *(Matrix a, int b)
+        {
+            if (b == 0) return null;
+            else
+            {
+                Matrix res = new Matrix(a.a.GetLength(0), a.a.GetLength(1));
+                for (int x = 0; x < a.a.GetLength(0); x++)
+                {
+                    for (int y = 0; y < a.a.GetLength(1); y++)
+                    {
+                        res.a[x, y] = a.a[x, y] * b;
+                    }
+                }
+                return res;
+            }
+        }
+        public static Matrix operator ~(Matrix a)
+        {
+            Matrix result = new Matrix(a.a.GetLength(1), a.a.GetLength(0));
+            for (int y = 0; y < a.a.GetLength(0); y++)
+            {
+                for (int x = 0; x < a.a.GetLength(1); x++)
+                {
+                    result.a[x, y] = a.a[y, x];
+                }
+            }
+            return result;
+        }
+        public override string ToString()
+        {
+            string str = "";
+            for (int x = 0; x < a.GetLength(0); x++)
+            {
+                for (int y = 0; y < a.GetLength(1); y++)
+                {
+                    str += $"{a[x, y],-5}";
+                }
+                str += "\n";
+            }
+            return str;
+        }
+
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,123 @@
+﻿using System
+
+namespace Lab4
+{
+    class Program
+    {
+        public static void Menu(Matrix first, Matrix second)
+        {
+            int choice;
+            do
+            {
+                choice = 666;
+                Console.WriteLine("Первая матрица:\n" + first.ToString());
+                
+                Console.WriteLine("Вторая матрица:\n" + second.ToString());
+                
+                Console.WriteLine("Выберите действие:\n1 - Сложение\n2 - Вычитание\n3 - Перемножение матриц\n4 - Умножение матрицы на число\n5 - Транспонирование матрицы\n0 - Выход из приложения\nВведите число, соответствующее вашему выбору:");
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                catch (System.FormatException)
+                {
+
+                }
+                switch (choice)
+                {
+                    case 1:
+                        //Сложение
+                        try
+                        {
+                            Matrix result = first + second;
+                            Console.WriteLine("Ответ:\n" + result.ToString());
+                        }
+                        catch (NullReferenceException)
+                        {
+                            Console.WriteLine("Ошибка: Матрицы неравного размера, операция невозможна");
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        //Вычитание
+                        try
+                        {
+                            Matrix result = first - second;
+                            Console.WriteLine("Ответ:\n" + result.ToString());
+                        }
+                        catch (NullReferenceException)
+                        {
+                            Console.WriteLine("Ошибка: Матрицы неравного размера, операция невозможна");
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 3:
+                        //Перемножение
+                        try
+                        {
+                            Matrix result = first * second;
+                            Console.WriteLine("Ответ:\n" + result.ToString());
+                        }
+                        catch (NullReferenceException)
+                        {
+                            Console.WriteLine("Ошибка: Матрицы неравного размера, операция невозможна");
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 4:
+                        //Умножение на число
+                        try
+                        {
+                            Console.WriteLine("Введите множитель:");
+                            int num = int.Parse(Console.ReadLine());
+
+                            if (num == 0)
+                            {
+                                throw new Exception("Вы ввели нулевое значение!");
+                            }
+
+                            Matrix result = first * num;
+                            Console.WriteLine("Ответ:\n" + result.ToString());
+                            
+                            result = second * num;
+                            Console.WriteLine("\n" + result.ToString());                            
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Ошибка: " + e.Message);
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 5:
+                        //Транспонирование 
+                        Matrix res_1 = ~first;
+                        Matrix res_2 = ~second;
+
+                        Console.WriteLine("Ответ:\n" + res_1.ToString() + "\n\n" + res_2.ToString());
+
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Ух-Ух! Похоже ошибочка вышла. Повторите ввод!");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+            } while (choice != 0);
+        }
+        static void Main(string[] args)
+        {
+            Matrix first = new Matrix(5, 3);
+            Matrix second = new Matrix(5, 3);
+            Menu(first, second);
+        }
+    }
+}
